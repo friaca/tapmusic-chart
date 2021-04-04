227995986ec76073ffad6542b55535778aa3d331
friaca <v.fria@outlook.com>
2021-04-04 17:59:52 -0300

    yay

---
+++ tapmusic.py
@@ -0,0 +1,75 @@
+from os.path import join, isfile
+import sys
+from urllib.request import urlopen
+import datetime
+
+def get_file_name():
+  today = datetime.date.today()
+  # If day <= 5, save chart as past month's
+  # Fallback to december if month is January
+  month = today.month - (today.day <= 5) or 12
+  year = today.year
+
+  return '{:02d}-{:02d}.jpg'.format(month, year)
+
+# ====== Constants ====== #
+URL = 'https://tapmusic.net/collage.php?'
+
+PERIODS_MAP = {
+  '7 days': '7days',
+  '1 month': '1month',
+  '3 months': '3months',
+  '6 months': '6months',
+  '12 months': '12months',
+  'overall': 'overall'
+}
+
+SIZES_MAP = {
+  '3x3': '3x3',
+  '4x4': '4x4',
+  '5x5': '5x5'
+}
+# ====== Constants end ====== #
+
+# Set your Last.FM username
+username = ''
+
+# On Windows don't forget to scape the slashes: 
+# i.e., C:\\Users\\User\\Pictures
+download_path = ''
+
+# `period` and `size` are default values, if changed, you may want to 
+# change `get_file_name` aswell, because that function only considers 
+# the "1 month" period, maybe I'll adapt this is the future...
+period = '1 month'
+size = '5x5'
+captions = True
+file_name = get_file_name()
+
+def main():
+  if '' in [username, download_path]:
+    print('Make sure `download_path` and `username` are filled')
+    sys.exit(1)
+
+  if period not in PERIODS_MAP.keys() or size not in SIZES_MAP.keys():
+    print('`period` and `size` must be a key of their respective maps')
+    sys.exit(1)
+
+  print(f'Download a {size} chart for user {username}, from the past {period}...')
+
+  path_joined = join(download_path, file_name)
+  full_path = f'{path_joined}_2' if isfile(path_joined) else path_joined
+
+  try:
+    with open(full_path, 'wb') as file:
+      final_url = f'{URL}user={username}&type={PERIODS_MAP[period]}&size={SIZES_MAP[size]}&caption={str(captions).lower()}'
+
+      with urlopen(final_url) as response:
+        file.write(response.read())
+
+    print('Downloaded succesfully!')
+  except Exception as e:
+    print("Couldn't download chart!\n[ERROR] ", e)
+
+if __name__ == '__main__':
+  main()
